1a7f187076d6552c41ab6e9539241377bdd889ec
kr4abhi <2019185@iiitdmj.ac.in>
2024-01-23 11:56:50 +0530

    loop imlpementation left

--- controllers/gmailControllers.js
+++ controllers/gmailControllers.js
@@ -1,10 +1,10 @@
 const { google } = require('googleapis');
 const gmailConfig = require('../config/gmailConfig');
 const { readTokens, writeTokens, TOKEN_PATH } = require('../tokens');
-const {checkIfReplied} = require('../utils/ifReplied');
+// const {checkIfReplied} = require('../utils/ifReplied');
 const { cloudchannel } = require('googleapis/build/src/apis/cloudchannel');
 
-const repliedUsers = new Set();
+// const repliedUsers = new Set();
 
 async function checkAndRespondToEmails(gmail) {
   try {
@@ -52,41 +52,102 @@ async function checkAndRespondToEmails(gmail) {
             id: message.threadId,
         });
 
-        console.log("thread found correctly");
+        console.log("thread found correctly", thread.data.id);
+        console.log(thread);
 
-        if (repliedUsers.has(thread)) {
-            console.log("Already replied to : ", From);
-            continue;
-          }
-        const replies = thread.data.messages.slice(1);
-        console.log("REPLY:  ", replies);
+        const replies = thread.data.messages;
 
-        if(replies.length === 0){
-            const replyRaw = await createReplyRaw(toEmail, From, subject);
+        const messageIds = replies.map((message)=>message.id);
+        console.log('Message IDs in the thread:', messageIds);
+
+
+// Check if any message in the thread was sent by the user
+        const userSentMessage = replies.some((message) => {
+        // Assuming the user's email address is stored in a variable called 'userEmailAddress'
+        return message.payload.headers.some((header) => {
+            return header.name === 'From' && header.value === toEmail;
+        });
+        });
+
+        if (!userSentMessage) {
+            // If no message in the thread was sent by the user, proceed to reply
+            const replyRaw = await createReplyRaw(toEmail, From, subject, message.threadId, messageIds);
+
             const sentReply = await gmail.users.messages.send({
-                userId: 'me',
-                requestBody: {
-                  raw: replyRaw,
-                },
-              });
-
-    
-
+              userId: 'me',
+              requestBody: {
+                raw: replyRaw,
+                threadId: message.threadId
+              },
+            });
+          
             const labelName = "onVacation";
+
             await gmail.users.messages.modify({
                 userId: 'me',
-                id: sentReply.data.id, // Use the ID of the sent reply, not the original message
+                id: sentReply.data.id,
                 requestBody: {
                   addLabelIds: [await createLabelIfNeeded(labelName, gmail)],
                 },
               });
+
+            await gmail.users.messages.modify({
+              userId: 'me',
+              id: message.threadId,
+              requestBody: {
+                addLabelIds: [await createLabelIfNeeded(labelName, gmail)],
+              },
+            });
+          
+            console.log("Sent reply to email:", From);
+          } else {
+            console.log("User has already sent a message in this thread.");
+            continue;
+          }
+
+        // const threadId = thread.data.id
+
+        // if (repliedUsers.has(thread)) {
+        //     console.log("Already replied to : ", From);
+        //     continue;
+        //   }
+
+        ///   making changes to reply logic
+
+        // const replies = thread.data.messages.slice(1);
+        // console.log("REPLY:  ", replies);
+
+        // if(replies.length === 0){
+        //     const replyRaw = await createReplyRaw(toEmail, From, subject);
+        //     const sentReply = await gmail.users.messages.send({
+        //         userId: 'me',
+        //         requestBody: {
+        //           raw: replyRaw,
+        //         },
+        //       });
+
+    
+
+        //     const labelName = "onVacation";
+        //     await gmail.users.messages.modify({
+        //         userId: 'me',
+        //         id: sentReply.data.id, // Use the ID of the sent reply, not the original message
+        //         requestBody: {
+        //           addLabelIds: [await createLabelIfNeeded(labelName, gmail)],
+        //         },
+        //       });
          
 
-              console.log("Sent reply to email:", From);
-          //Add the user to replied users set
+        //       console.log("Sent reply to email:", From);
+        //   //Add the user to replied users set
               
-        }
-        repliedUsers.add(thread);
+        // }
+
+        
+
+         ///   making changes to reply logic
+
+        // repliedUsers.add(thread);
         // to comment 
 
         // // console.log('New Email from:', senderEmail);
@@ -103,15 +164,16 @@ async function checkAndRespondToEmails(gmail) {
   }
 }
 
-async function createReplyRaw(from, to, subject) {
-    const emailContent = `From: ${from}\nTo: ${to}\nSubject: ${subject}\n\nThank you for your message. i am  unavailable right now, but will respond as soon as possible...`;
+async function createReplyRaw(from, to, subject, threadId, messageIds) {
+    const referencesHeader = messageIds.map((id) => `<${id}>`).join(' ');
+    const emailContent = `From: ${from}\nTo: ${to}\nSubject: Re: ${subject}\nIn-Reply-To: <${threadId}>\nReferences: ${referencesHeader}\n\nThank you for your message. I am unavailable right now, but will respond as soon as possible...`;
     const base64EncodedEmail = Buffer.from(emailContent)
       .toString("base64")
       .replace(/\+/g, "-")
       .replace(/\//g, "_");
-//   console.log(base64EncodedEmail);
     return base64EncodedEmail;
   }
+  
 
 async function createLabelIfNeeded(labelName, gmail) {
     // const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
@@ -153,3 +215,6 @@ async function createLabelIfNeeded(labelName, gmail) {
 module.exports = {
   checkAndRespondToEmails,
 };
+
+
+
--- routes/gmailRouter.js
+++ routes/gmailRouter.js
@@ -43,45 +43,6 @@ try {
     console.error('Error checking emails:', error.message);
     res.status(500).send('Error checking emails.');
   }
-
-//   try {
-//     // List messages in the user's inbox
-//     const response = await gmail.users.messages.list({
-//       userId: 'me',
-//       labelIds: ['INBOX'],
-//     });
-
-//     const messages = response.data.messages || [];
-    
-//     if (messages.length === 0) {
-//       return res.send('No new emails.');
-//     }
-
-//     // Process each message, check for prior replies, and send auto-replies as needed
-//     for (const message of messages) {
-//       // Implement logic to check for prior replies and send auto-replies
-//       // This is where you'll implement steps 2, 3, and 4
-//       // ...
-
-//       // For now, let's just log the message ID
-//       console.log('New Email ID:', message.id);
-
-//       const messageDetails = await gmail.users.messages.get({
-//         userId: 'me',
-//         id: message.id,
-//       });
-  
-//       const senderEmail = messageDetails.data.payload.headers.find(header => header.name === 'From').value;
-    
-//     // Now you have the sender's email address (senderEmail)
-//     console.log('New Email from:', senderEmail);
-//     }
-
-//     res.send('Email checking completed.');
-//   } catch (error) {
-//     console.error('Error checking emails:', error.message);
-//     res.status(500).send('Error checking emails.');
-//   }
 });
 
 module.exports = router;
